73e8a78614782d7424da60ca023847c47c3e3ecc
SUMANASHRI <101055727+SUMANASHRI@users.noreply.github.com>
2022-04-10 19:02:51 +0530

    Add files via upload

---
+++ my captain .py
@@ -0,0 +1,14 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def area_of_circle(r):
+    pi = 3.14
+    area = pi * r * r
+    return area
+
+print(area_of_circle(5))
+78.5
+filename = input("abc.py")
+abc.py
+f_extns = filename.split(".")
+print("the extension of the file is :" + repr(f_extns[-1]))
+the extension of the file is :"python"
